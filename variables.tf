de553d26143722f19c5fb3a374bb06b00cf916fe
Mannmeet <manmeet.a.jb@gmail.com>
2024-09-30 17:58:22 +0530

    Terraform Practice Project

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-
+variable "vpc" {}
+variable "env" {}
+variable "apps" {}
+variable "bastion_nodes" {}
+variable "ami" {}
+variable "instance_type" {}
 # variable "db" {}
 # variable "vault_token" {}
 # variable "zone_id" {}
