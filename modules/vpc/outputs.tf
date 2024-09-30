a9ebefbcaf3065aa992399064299e0c062593d10
Mannmeet <manmeet.a.jb@gmail.com>
2024-09-30 22:27:28 +0530

    Terraform Practice Project

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,10 +1,10 @@
 output "subnets" {
-  value = tomap(
-    "web"       = aws_subnet.web_subnet.*.id,
-    "app"       = aws_subnet.app_subnet.*.id,
-    "db"        = aws_subnet.db_subnet.*.id,
-    "public"    = aws_subnet.public_subnet.*.id
-  )
+  value = tomap({
+    "web"    = aws_subnet.web_subnet.*.id
+    "app"    = aws_subnet.app_subnet.*.id
+    "db"     = aws_subnet.db_subnet.*.id
+    "public" = aws_subnet.public_subnet.*.id
+  })
 }
 
 output "vpc_id" {
