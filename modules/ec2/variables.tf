d3b4b768c049e7c7f39e478167034fafbb072dcb
Mannmeet <manmeet.a.jb@gmail.com>
2024-09-30 17:56:21 +0530

    Terraform Practice Project

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,9 @@
-variable "ss" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "env" {}
+variable "name" {}
+variable "vpc_id" {}
+variable "bastion_nodes" {}
+variable "allow_port" {}
+variable "allow_sg_cidr" {}
+variable "subnet_ids" {}
