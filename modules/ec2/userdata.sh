ce9439bf94854a21d0688c2452e07b3d2f0e5063
Mannmeet <manmeet.a.jb@gmail.com>
2024-10-04 20:41:20 +0530

    Terraform Practice Project

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,4 +1,4 @@
-$/bin/bash
+#!/bin/bash
 
 pip3.11 install ansible 2>&1 | tee -a /opt/userdata.log
 ansible_pull -i localhost, -U https://github.com/MannmeetOrg/roboshop-ansible.git -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
