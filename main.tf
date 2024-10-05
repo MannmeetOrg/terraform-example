2751f1bba57393fbbabf3bbf1b3a2ce8b723eaa7
Mannmeet <manmeet.a.jb@gmail.com>
2024-10-05 19:17:28 +0530

    Terraform Practice Project

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ module "vpc" {
 }
 
 module "apps" {
+  depends_on = [module.db, module.vpc]
   source = "./modules/ec2"
 
   for_each      = var.apps
@@ -32,6 +33,7 @@ module "apps" {
 }
 
 module "db" {
+  depends_on = [module.vpc]
   source = "./modules/ec2"
 
   for_each      = var.db
