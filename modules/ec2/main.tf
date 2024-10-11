58ee38653ba59f3d0c4ee363f7233ffa12eb4ad0
Mannmeet <manmeet.a.jb@gmail.com>
2024-10-11 20:06:30 +0530

    Terraform code Modified

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -151,7 +151,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.main.arn
+  load_balancer_arn = aws_lb.main.*.arn[count.index]
   port              = "80"
   protocol          = "HTTP"
 
