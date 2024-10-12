4cf49a68fa46ac97b235decc381a7064c339b11a
Mannmeet <manmeet.a.jb@gmail.com>
2024-10-12 21:54:53 +0530

    Terraform code Modified

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -63,7 +63,7 @@ resource "aws_autoscaling_group" "main" {
   min_size            = var.capacity["min"]
   vpc_zone_identifier = var.subnet_ids
   # target_group_arns   = [aws_lb_target_group.main.*.arn[count.index]]
-  load_balancers      = [aws_lb.main.*.arn[count.index]]
+  # load_balancers      = [aws_lb.main.*.arn[count.index]]
 
   launch_template {
     id      = aws_launch_template.main[0].id
